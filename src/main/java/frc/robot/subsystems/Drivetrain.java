728e254f331c09b1314a94f921d988a56e43f735
BreakerBots <132958980+BreakerBots-public@users.noreply.github.com>
2024-10-08 18:55:49 -0700

    ...

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.BreakerLib.swerve.BreakerSwerveDrivetrain;
+import static frc.robot.Constants.DriveConstants.*;
+
+public class Drivetrain extends BreakerSwerveDrivetrain {
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    super(DRIVETRAIN_CONSTANTS, FRONT_LEFT_MODULE_CONSTANTS, FRONT_RIGHT_MODULE_CONSTANTS, BACK_LEFT_MODULE_CONSTANTS, BACK_RIGHT_MODULE_CONSTANTS);
+  }
+}
